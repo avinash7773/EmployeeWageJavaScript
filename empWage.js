eb301e4132847aebce15de152bdb7e5746d3cfaa
avinash7773 <javinash228@gmail.com>
2021-05-26 21:26:08 +0530

    add to check month wage

--- empWage.js
+++ empWage.js
@@ -4,6 +4,13 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
+const NUM_OF_WORKING_DAYS = 20;
+let empHrs =0;
+for(let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
+
 function getWorkingHours(empCheck) {
     switch(empCheck) {
         case IS_PART_TIME:
@@ -16,10 +23,8 @@ function getWorkingHours(empCheck) {
             empHrs = 0;
             break;
     }
+    return empHrs;
 }
 
-let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee Wage="+empWage);
+console.log("Total Hrs: "+ empHrs +" Emp wage: "+ empWage);
