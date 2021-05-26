095877b245a16458c5b5441dad9f84cc64e39f5e
avinash7773 <javinash228@gmail.com>
2021-05-26 21:39:40 +0530

    add to check total wage till 20 days or 100 hours reached

--- empWage.js
+++ empWage.js
@@ -4,11 +4,15 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-const NUM_OF_WORKING_DAYS = 20;
-let empHrs =0;
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+const NUM_OF_WORKING_DAYS = 20
+const MAX_HOURS_IN_MONTH = 100;;
+
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while(totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empCheck);
+    totalEmpHrs += getWorkingHours(empCheck);
 }
 
 function getWorkingHours(empCheck) {
@@ -26,5 +30,5 @@ function getWorkingHours(empCheck) {
     return empHrs;
 }
 
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Hrs: "+ empHrs +" Emp wage: "+ empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Days: "+totalWorkingDays+" Total Hrs: "+ totalEmpHrs +" Emp wage: "+ empWage);
